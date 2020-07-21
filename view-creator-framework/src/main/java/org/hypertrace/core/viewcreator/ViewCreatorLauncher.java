987fcbf2060ba93ebb331191b89dc0e18cacdf20
rleiwang-traceableai <rock@traceable.ai>
2020-07-21 10:19:20 -0700

    remove content from stop() method

--- view-creator-framework/src/main/java/org/hypertrace/core/viewcreator/ViewCreatorLauncher.java
+++ view-creator-framework/src/main/java/org/hypertrace/core/viewcreator/ViewCreatorLauncher.java
@@ -2,22 +2,15 @@ package org.hypertrace.core.viewcreator;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.typesafe.config.Config;
-import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.List;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
 import org.hypertrace.core.serviceframework.PlatformService;
 import org.hypertrace.core.serviceframework.config.ConfigClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /** ViewCreatorLauncher creates Pinot tables as specified in the config. */
 public class ViewCreatorLauncher extends PlatformService {
-  private static final Logger LOGGER = LoggerFactory.getLogger(ViewCreatorLauncher.class);
   private static final String TOOL_CLASS = "tool.class";
   private static final String VIEWS = "views";
   private static final String SERVICE_NAME_CONFIG = "service.name";
@@ -43,17 +36,7 @@ public class ViewCreatorLauncher extends PlatformService {
   }
 
   @Override
-  protected void doStop() {
-    String istioPilotQuitEndpoint = "http://127.0.0.1:15020/quitquitquit";
-    HttpClient httpclient = HttpClients.createDefault();
-    HttpPost httppost = new HttpPost(istioPilotQuitEndpoint);
-    try {
-      httpclient.execute(httppost);
-      LOGGER.info("Request to pilot succeeded");
-    } catch (IOException e) {
-      LOGGER.error("Error while calling quitquitquit", e);
-    }
-  }
+  protected void doStop() {}
 
   @Override
   public boolean healthCheck() {
