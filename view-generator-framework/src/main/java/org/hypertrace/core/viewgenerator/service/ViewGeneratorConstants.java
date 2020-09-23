a9da31546e70ffc60258e861534ca72c5400eb0b
Ronak <ronak@traceable.ai>
2020-09-23 19:16:13 +0530

    removed un-used constants

--- view-generator-framework/src/main/java/org/hypertrace/core/viewgenerator/service/ViewGeneratorConstants.java
+++ view-generator-framework/src/main/java/org/hypertrace/core/viewgenerator/service/ViewGeneratorConstants.java
@@ -1,12 +1,9 @@
 package org.hypertrace.core.viewgenerator.service;
 
 public class ViewGeneratorConstants {
-  public static final String SCHEMA_REGISTRY_CONFIG_KEY = "schema.registry.config";
   public static final String KAFKA_STREAMS_CONFIG_KEY = "kafka.streams.config";
-  public static final String JOB_CONFIG = "jobconfig";
   public static final String INPUT_TOPIC_CONFIG_KEY = "input.topic";
   public static final String OUTPUT_TOPIC_CONFIG_KEY = "output.topic";
   public static final String VIEW_GENERATOR_CLASS_CONFIG_KEY = "view.generator.class";
-  public static final String INPUT_CLASS_CONFIG_KEY = "input.class";
   public static final String VIEW_GENERATORS_CONFIG = "view.generators";
 }
