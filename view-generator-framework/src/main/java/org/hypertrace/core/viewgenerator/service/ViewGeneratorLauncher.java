9bcd57a973bdfe0c66dd87e22be22e9af1af3c28
Ronak <ronak@traceable.ai>
2020-09-30 18:21:34 +0530

    addressed review comments

--- view-generator-framework/src/main/java/org/hypertrace/core/viewgenerator/service/ViewGeneratorLauncher.java
+++ view-generator-framework/src/main/java/org/hypertrace/core/viewgenerator/service/ViewGeneratorLauncher.java
@@ -14,8 +14,11 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import org.hypertrace.core.kafkastreams.framework.KafkaStreamsApp;
 import org.hypertrace.core.serviceframework.config.ConfigClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ViewGeneratorLauncher extends KafkaStreamsApp {
+  private static final Logger logger = LoggerFactory.getLogger(ViewGeneratorLauncher.class);
 
   private static final String DEFAULT_VIEW_GEN_JOB_CONFIG_KEY = "view-gen-job-config-key";
   private String viewGenName;
@@ -55,19 +58,20 @@ public class ViewGeneratorLauncher extends KafkaStreamsApp {
       inputStreams.put(inputTopic, inputStream);
     }
 
-    // This enviorn property helps in overriding producer value serde.
-    // In local mode of deployment of single ingestion pipeline, this helps
-    // in using GenericAvroSerde which is independent of schema registry.
+    // This environment property helps in overriding producer value serde. For hypertrace quickstart
+    // deployment, this helps in using GenericAvroSerde for pinot views.
     Serde producerValueSerde = null;
-    String envProducerValueSerdeClassName = System.getProperty("PRODUCER_VALUE_SERDE");
+    String envProducerValueSerdeClassName = System.getenv("PRODUCER_VALUE_SERDE");
     if (envProducerValueSerdeClassName != null) {
       try {
+        logger.info("Using producer value serde: {}", envProducerValueSerdeClassName);
         Class clazz = Class.forName(envProducerValueSerdeClassName);
-        producerValueSerde = Serdes.serdeFrom(clazz);
+        producerValueSerde = (Serde) clazz.getDeclaredConstructor().newInstance();
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
     }
+
     inputStream.flatMapValues(viewMapper)
         .to(outputTopic, Produced.with(Serdes.String(), producerValueSerde));
     return streamsBuilder;
