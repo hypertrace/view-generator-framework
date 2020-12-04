67c7b032e537884d507bf1ec79280e9ed4ea67c4
Laxman Ch <laxman@traceable.ai>
2020-12-04 17:30:58 +0530

    Pinot 0.6 upgrade

--- view-creator-framework/build.gradle.kts
+++ view-creator-framework/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
   implementation("org.apache.avro:avro:1.9.2")
   implementation("org.apache.kafka:kafka-clients:5.5.0-ccs")
-  implementation("org.apache.pinot:pinot-tools:0.5.0") {
+  implementation("org.apache.pinot:pinot-tools:0.6.0") {
     // We use newer version of servlet-api brought by jetty so we exclude the older
     // version here.
     exclude("javax.servlet", "javax.servlet-api")
