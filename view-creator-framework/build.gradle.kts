db9277f2483d5b5bbff2aee25cd6766a465b892e
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2021-04-05 05:05:08 -0700

    chore: update dependencies to fix vulns (#29)

--- view-creator-framework/build.gradle.kts
+++ view-creator-framework/build.gradle.kts
@@ -19,9 +19,9 @@ tasks.test {
 }
 
 dependencies {
-  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.21")
+  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.23")
   constraints {
-    implementation("io.netty:netty-all:4.1.54.Final") {
+    implementation("io.netty:netty-all:4.1.63.Final") {
       because("HTTP Request Smuggling [High Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-559515] in io.netty:netty-all@4.1.28.Final")
     }
     implementation("commons-collections:commons-collections:3.2.2") {
@@ -33,9 +33,6 @@ dependencies {
     implementation("org.webjars:swagger-ui:3.27.0") {
       because("Reverse Tabnabbing [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-ORGWEBJARS-449821] in org.webjars:swagger-ui@2.2.2")
     }
-    implementation("org.yaml:snakeyaml:1.26") {
-      because("Denial of Service (DoS) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-ORGYAML-537645] in org.yaml:snakeyaml@1.23")
-    }
     implementation("org.apache.zookeeper:zookeeper:3.6.1") {
       because("Access Control Bypass [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-ORGAPACHEZOOKEEPER-174781] in org.apache.zookeeper:zookeeper@3.4.13")
     }
@@ -45,32 +42,29 @@ dependencies {
     implementation("org.glassfish.jersey.core:jersey-server:2.31") {
       because("XML Entity Expansion [High Severity][https://snyk.io/vuln/SNYK-JAVA-ORGGLASSFISHJERSEYMEDIA-595972] in org.glassfish.jersey.media:jersey-media-jaxb@2.28")
     }
-    implementation("io.grpc:grpc-core:1.33.1") {
-      because("Information Exposure [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IOGRPC-571957] in io.grpc:grpc-core@1.30.0")
-    }
-    implementation("com.google.guava:guava:30.0-jre") {
-      because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415] in com.google.guava:guava@29.0-android")
-    }
-    // We put a constraint on calcite-babel instead of calcite-core so that all of calcite is upgraded.
-    implementation("org.apache.calcite:calcite-babel:1.26.0") {
-      because("Man-in-the-Middle (MitM) [High Severity][https://snyk.io/vuln/SNYK-JAVA-ORGAPACHECALCITE-1038296] in org.apache.calcite:calcite-core@1.19.0")
-    }
-    implementation("org.apache.httpcomponents:httpclient:4.5.13") {
-      because("Improper Input Validation [High Severity][https://snyk.io/vuln/SNYK-JAVA-ORGAPACHEHTTPCOMPONENTS-1048058] in org.apache.httpcomponents:httpclient@4.5.9")
-    }
-    implementation("io.netty:netty-handler:4.1.59.Final") {
-      because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082235] in io.netty:netty-handler@4.1.48.Final")
-      because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082234] in io.netty:netty-common@4.1.48.Final")
-      because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082236] in io.netty:netty-transport@4.1.48.Final")
-    }
-    implementation("io.netty:netty-transport-native-epoll:4.1.59.Final") {
-      because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082234] in io.netty:netty-common@4.1.48.Final")
-      because(" Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082236] in io.netty:netty-transport@4.1.48.Final")
-      because(" Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082238] in io.netty:netty-transport-native-epoll@4.1.48.Final")
-    }
+      implementation("io.grpc:grpc-core:1.36.1") {
+        because("Information Exposure [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IOGRPC-571957] in io.grpc:grpc-core@1.30.0")
+      }
+      // We put a constraint on calcite-babel instead of calcite-core so that all of calcite is upgraded.
+      implementation("org.apache.calcite:calcite-babel:1.26.0") {
+        because("Man-in-the-Middle (MitM) [High Severity][https://snyk.io/vuln/SNYK-JAVA-ORGAPACHECALCITE-1038296] in org.apache.calcite:calcite-core@1.19.0")
+      }
+      implementation("org.apache.httpcomponents:httpclient:4.5.13") {
+        because("Improper Input Validation [High Severity][https://snyk.io/vuln/SNYK-JAVA-ORGAPACHEHTTPCOMPONENTS-1048058] in org.apache.httpcomponents:httpclient@4.5.9")
+      }
+      implementation("io.netty:netty-handler:4.1.63.Final") {
+        because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082235] in io.netty:netty-handler@4.1.48.Final")
+        because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082234] in io.netty:netty-common@4.1.48.Final")
+        because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082236] in io.netty:netty-transport@4.1.48.Final")
+      }
+      implementation("io.netty:netty-transport-native-epoll:4.1.63.Final") {
+        because("Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082234] in io.netty:netty-common@4.1.48.Final")
+        because(" Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082236] in io.netty:netty-transport@4.1.48.Final")
+        because(" Information Disclosure (new) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-IONETTY-1082238] in io.netty:netty-transport-native-epoll@4.1.48.Final")
+      }
   }
 
-  implementation("org.apache.avro:avro:1.9.2")
+  implementation("org.apache.avro:avro:1.10.2")
   implementation("org.apache.kafka:kafka-clients:5.5.0-ccs")
   implementation("org.apache.pinot:pinot-tools:0.6.0") {
     // We use newer version of servlet-api brought by jetty so we exclude the older
@@ -89,8 +83,8 @@ dependencies {
   implementation("com.typesafe:config:1.4.1")
   runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.14.0")
 
-  testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
-  testImplementation("org.mockito:mockito-core:3.6.28")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+  testImplementation("org.mockito:mockito-core:3.8.0")
 }
 
 group = "org.hypertrace.core.viewcreator"
