2207ac4522715d75536c674b1d6e7f985daa985f
Ravi Singal <ravi.singal@traceable.ai>
2021-04-09 00:29:47 +0530

    update version-settings plugin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 plugins {
-  id("org.hypertrace.version-settings") version "0.1.1"
+  id("org.hypertrace.version-settings") version "0.2.0"
 }
 
 include(":view-generator-framework")
