95c376407238b8a4c3510256b643626ee31fb5f0
Ravi Singal <ravi.singal@traceable.ai>
2021-08-08 23:14:20 +0530

    fix synk issues

--- view-creator-framework/build.gradle.kts
+++ view-creator-framework/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     implementation("com.google.guava:guava:30.0-android") {
       because("Information Disclosure [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415] in com.google.guava:guava@28.2-android")
     }
+    implementation("org.apache.commons:commons-compress:1.21") {
+      because("Denial of Service (DoS) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-ORGAPACHECOMMONS-1316640] in org.apache.commons:commons-compress@1.20")
+    }
   }
 
   compileOnly("org.projectlombok:lombok:1.18.20")
