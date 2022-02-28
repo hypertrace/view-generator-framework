faa990dd39b43786f893b9b385cfec9556fc510b
Laxman Ch <laxman@traceable.ai>
2022-02-28 19:58:37 +0530

    preprocessing support for view generation

--- view-generator-framework/src/main/java/org/hypertrace/core/viewgenerator/ViewGenerator.java
+++ view-generator-framework/src/main/java/org/hypertrace/core/viewgenerator/ViewGenerator.java
@@ -7,7 +7,7 @@ import org.apache.avro.generic.GenericRecord;
 public interface ViewGenerator<T extends GenericRecord> {
 
   /** Configure the view generator */
-  public default void configure(Config config) {}
+  default void configure(Config config) {};
 
   /** Name of the generated View. This should be unique across entire name space. */
   String getViewName();
