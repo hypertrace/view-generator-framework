f2424384504b097e00b4b4a7c57358466dcc25b1
Laxman Ch <laxman@traceable.ai>
2022-03-03 15:44:55 +0530

    add kafka topology unit test

--- view-generator-framework/src/test/java/org/hypertrace/core/viewgenerator/ViewGeneratorLauncherTest.java
+++ view-generator-framework/src/test/java/org/hypertrace/core/viewgenerator/ViewGeneratorLauncherTest.java
@@ -1,21 +1,21 @@
 package org.hypertrace.core.viewgenerator;
 
-import static org.hypertrace.core.viewgenerator.service.ViewGeneratorConstants.DEFAULT_VIEW_GEN_JOB_CONFIG_KEY;
-import static org.hypertrace.core.viewgenerator.service.ViewGeneratorConstants.INPUT_TOPIC_CONFIG_KEY;
+import static org.hypertrace.core.viewgenerator.service.ViewGeneratorConstants.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.TestInputTopic;
-import org.apache.kafka.streams.TopologyTestDriver;
+import org.apache.kafka.streams.*;
 import org.hypertrace.core.kafkastreams.framework.serdes.AvroSerde;
 import org.hypertrace.core.serviceframework.config.ConfigClientFactory;
 import org.hypertrace.core.viewgenerator.service.ViewGeneratorLauncher;
 import org.hypertrace.core.viewgenerator.test.api.SpanTypeOne;
+import org.hypertrace.core.viewgenerator.test.api.SpanTypeTwo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
@@ -23,16 +23,13 @@ import org.junitpioneer.jupiter.SetEnvironmentVariable;
 public class ViewGeneratorLauncherTest {
   private static final String SERVICE_NAME = "servicename";
   private ViewGeneratorLauncher underTest;
+  TestInputTopic<byte[], SpanTypeOne> inputTopic;
+  TestOutputTopic<byte[], SpanTypeTwo> outputTopic;
 
   @BeforeEach
   @SetEnvironmentVariable(key = "SERVICE_NAME", value = "test-view-generator")
   public void setUp() {
     underTest = new ViewGeneratorLauncher(ConfigClientFactory.getClient());
-  }
-
-  @Test
-  @SetEnvironmentVariable(key = "SERVICE_NAME", value = "test-view-generator")
-  public void whenJaegerSpansAreProcessedExpectRawSpansToBeOutput() {
     Config config =
         ConfigFactory.parseURL(
             getClass()
@@ -51,144 +48,50 @@ public class ViewGeneratorLauncherTest {
     mergedProps.forEach(props::put);
 
     TopologyTestDriver td = new TopologyTestDriver(streamsBuilder.build(), props);
-    TestInputTopic<byte[], SpanTypeOne> inputTopic =
+
+    Serde<SpanTypeOne> spanTypeOneSerde = new AvroSerde<>();
+    spanTypeOneSerde.configure(Map.of(), false);
+
+    Serde<SpanTypeTwo> spanTypeTwoSerde = new AvroSerde<>();
+    spanTypeTwoSerde.configure(Map.of(), false);
+
+    inputTopic =
         td.createInputTopic(
             config.getString(INPUT_TOPIC_CONFIG_KEY),
             Serdes.ByteArray().serializer(),
-            new JaegerSpanSerde().serializer());
-
-    Serde<RawSpan> rawSpanSerde = new AvroSerde<>();
-    rawSpanSerde.configure(Map.of(), false);
-
-    Serde<TraceIdentity> spanIdentitySerde = new AvroSerde<>();
-    spanIdentitySerde.configure(Map.of(), true);
-
-    TestOutputTopic outputTopic =
+            spanTypeOneSerde.serializer());
+    outputTopic =
         td.createOutputTopic(
-            config.getString(SpanNormalizerConstants.OUTPUT_TOPIC_CONFIG_KEY),
-            spanIdentitySerde.deserializer(),
-            rawSpanSerde.deserializer());
+            config.getString(OUTPUT_TOPIC_CONFIG_KEY),
+            Serdes.ByteArray().deserializer(),
+            spanTypeTwoSerde.deserializer());
+  }
 
-    TestOutputTopic rawLogOutputTopic =
-        td.createOutputTopic(
-            config.getString(SpanNormalizerConstants.OUTPUT_TOPIC_RAW_LOGS_CONFIG_KEY),
-            spanIdentitySerde.deserializer(),
-            new AvroSerde<>().deserializer());
+  @Test
+  @SetEnvironmentVariable(key = "SERVICE_NAME", value = "test-view-generator")
+  public void testViewGeneratorTopology() {
+    long spanStartTime = System.currentTimeMillis();
+    long spanEndTime = spanStartTime + 1000;
 
-    Span span =
-        Span.newBuilder()
-            .setSpanId(ByteString.copyFrom("1".getBytes()))
-            .setTraceId(ByteString.copyFrom("trace-1".getBytes()))
-            .addTags(
-                JaegerSpanInternalModel.KeyValue.newBuilder()
-                    .setKey("jaeger.servicename")
-                    .setVStr(SERVICE_NAME)
-                    .build())
-            .addLogs(
-                Log.newBuilder()
-                    .setTimestamp(Timestamp.newBuilder().setSeconds(5).build())
-                    .addFields(
-                        JaegerSpanInternalModel.KeyValue.newBuilder()
-                            .setKey("e1")
-                            .setVStr("some event detail")
-                            .build())
-                    .addFields(
-                        JaegerSpanInternalModel.KeyValue.newBuilder()
-                            .setKey("e2")
-                            .setVStr("some event detail")
-                            .build()))
-            .addLogs(
-                Log.newBuilder()
-                    .setTimestamp(Timestamp.newBuilder().setSeconds(10).build())
-                    .addFields(
-                        JaegerSpanInternalModel.KeyValue.newBuilder()
-                            .setKey("z2")
-                            .setVStr("some event detail")
-                            .build()))
-            .build();
-    inputTopic.pipeInput(span);
-
-    KeyValue<TraceIdentity, RawSpan> kv = outputTopic.readKeyValue();
-    assertEquals("__default", kv.key.getTenantId());
-    assertEquals(
-        HexUtils.getHex(ByteString.copyFrom("trace-1".getBytes()).toByteArray()),
-        HexUtils.getHex(kv.key.getTraceId().array()));
-    RawSpan value = kv.value;
-    assertEquals(HexUtils.getHex("1".getBytes()), HexUtils.getHex((value).getEvent().getEventId()));
-    assertEquals(SERVICE_NAME, value.getEvent().getServiceName());
-
-    KeyValue<String, LogEvents> keyValue = rawLogOutputTopic.readKeyValue();
-    LogEvents logEvents = keyValue.value;
-    Assertions.assertEquals(2, logEvents.getLogEvents().size());
-
-    // pipe in one more span which doesn't match spanDropFilters
-    Span span2 =
-        Span.newBuilder()
-            .setSpanId(ByteString.copyFrom("2".getBytes()))
-            .setTraceId(ByteString.copyFrom("trace-2".getBytes()))
-            .addTags(
-                JaegerSpanInternalModel.KeyValue.newBuilder()
-                    .setKey("jaeger.servicename")
-                    .setVStr(SERVICE_NAME)
-                    .build())
-            .addTags(
-                JaegerSpanInternalModel.KeyValue.newBuilder()
-                    .setKey("http.method")
-                    .setVStr("GET")
-                    .build())
+    SpanTypeOne span =
+        SpanTypeOne.newBuilder()
+            .setSpanId("span-id")
+            .setSpanKind("span-kind")
+            .setStartTimeMillis(spanStartTime)
+            .setEndTimeMillis(spanEndTime)
             .build();
 
-    inputTopic.pipeInput(span2);
-    KeyValue<TraceIdentity, RawSpan> kv1 = outputTopic.readKeyValue();
-    assertNotNull(kv1);
-    assertEquals("__default", kv1.key.getTenantId());
-    assertEquals(
-        HexUtils.getHex(ByteString.copyFrom("trace-2".getBytes()).toByteArray()),
-        HexUtils.getHex(kv1.key.getTraceId().array()));
+    Serde<SpanTypeOne> spanTypeOneSerde = new AvroSerde<>();
+    spanTypeOneSerde.configure(Map.of(), false);
 
-    // pipe in one more span which match one of spanDropFilters (http.method & http.url)
-    Span span3 =
-        Span.newBuilder()
-            .setSpanId(ByteString.copyFrom("3".getBytes()))
-            .setTraceId(ByteString.copyFrom("trace-3".getBytes()))
-            .addTags(
-                JaegerSpanInternalModel.KeyValue.newBuilder()
-                    .setKey("jaeger.servicename")
-                    .setVStr(SERVICE_NAME)
-                    .build())
-            .addTags(
-                JaegerSpanInternalModel.KeyValue.newBuilder()
-                    .setKey("http.method")
-                    .setVStr("GET")
-                    .build())
-            .addTags(
-                JaegerSpanInternalModel.KeyValue.newBuilder()
-                    .setKey("http.url")
-                    .setVStr("http://xyz.com/health/check")
-                    .build())
-            .build();
+    // assertNotNull(spanTypeOneSerde.serializer().serialize("test-input-topic", span));
+    inputTopic.pipeInput(null, span);
 
-    inputTopic.pipeInput(span3);
-    assertTrue(outputTopic.isEmpty());
-
-    // pipe in one more span which match one of spanDropFilters (grpc.url)
-    Span span4 =
-        Span.newBuilder()
-            .setSpanId(ByteString.copyFrom("3".getBytes()))
-            .setTraceId(ByteString.copyFrom("trace-3".getBytes()))
-            .addTags(
-                JaegerSpanInternalModel.KeyValue.newBuilder()
-                    .setKey("jaeger.servicename")
-                    .setVStr(SERVICE_NAME)
-                    .build())
-            .addTags(
-                JaegerSpanInternalModel.KeyValue.newBuilder()
-                    .setKey("grpc.url")
-                    .setVStr("doesn't match with input filter set")
-                    .build())
-            .build();
-
-    inputTopic.pipeInput(span4);
-    assertTrue(outputTopic.isEmpty());
+    KeyValue<byte[], SpanTypeTwo> kv = outputTopic.readKeyValue();
+    assertNull(kv.key, "null key expected");
+    assertEquals("span-id", kv.value.getSpanId());
+    assertEquals("span-kind", kv.value.getSpanKind());
+    assertEquals(spanStartTime, kv.value.getStartTimeMillis());
+    assertEquals(spanStartTime, kv.value.getEndTimeMillis());
   }
 }
