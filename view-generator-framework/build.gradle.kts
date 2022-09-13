202968ff90dda301beca22a24b1661006c73d6c1
Laxman Ch <60599147+laxmanchekka@users.noreply.github.com>
2022-09-13 17:46:23 +0530

    kafka upgrade (#68)

--- view-generator-framework/build.gradle.kts
+++ view-generator-framework/build.gradle.kts
@@ -12,7 +12,7 @@ tasks.test {
 
 dependencies {
   implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.43")
-  implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-framework:0.1.26")
+  implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-framework:0.2.0")
   implementation("org.apache.avro:avro:1.11.1")
   implementation("com.typesafe:config:1.4.2")
   implementation("com.google.guava:guava:31.1-jre")
@@ -20,24 +20,10 @@ dependencies {
   // Logging
   implementation("org.slf4j:slf4j-api:1.7.36")
 
-  constraints {
-    implementation("org.glassfish.jersey.core:jersey-common:2.34") {
-      because("Information Disclosure [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-ORGGLASSFISHJERSEYCORE-1255637] in org.glassfish.jersey.core:jersey-common@2.30")
-    }
-    implementation("org.apache.commons:commons-compress:1.21") {
-      because("Denial of Service (DoS) [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-ORGAPACHECOMMONS-1316640] in org.apache.commons:commons-compress@1.20")
-    }
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.1") {
-      because("Denial of Service (DoS) [High Severity]" +
-          "[https://snyk.io/vuln/SNYK-JAVA-COMFASTERXMLJACKSONCORE-2421244] " +
-          "in com.fasterxml.jackson.core:jackson-databind@2.13.1")
-    }
-  }
-
-  testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-  testImplementation("org.junit-pioneer:junit-pioneer:1.7.0")
-  testImplementation("org.mockito:mockito-core:4.5.1")
-  testImplementation("org.apache.kafka:kafka-streams-test-utils:6.0.1-ccs")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+  testImplementation("org.junit-pioneer:junit-pioneer:1.7.1")
+  testImplementation("org.mockito:mockito-core:4.7.0")
+  testImplementation("org.apache.kafka:kafka-streams-test-utils:7.2.1-ccs")
   testRuntimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
 }
 
