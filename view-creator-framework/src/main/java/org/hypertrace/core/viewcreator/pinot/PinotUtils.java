a10917d26c03118dbf91c9c72d40dab30236d83a
Ronak <ronak@traceable.ai>
2023-02-24 12:53:04 +0530

    fixed the compilation issue

--- view-creator-framework/src/main/java/org/hypertrace/core/viewcreator/pinot/PinotUtils.java
+++ view-creator-framework/src/main/java/org/hypertrace/core/viewcreator/pinot/PinotUtils.java
@@ -439,7 +439,7 @@ public class PinotUtils {
                 transformConfig.getString(PINOT_TRANSFORM_COLUMN_FUNCTION)));
       }
     }
-    return new IngestionConfig(null, null, null, tableTransformConfigs, null);
+    return new IngestionConfig(null, null, null, tableTransformConfigs);
   }
 
   private static TagOverrideConfig toTagOverrideConfig(Config tenantTagOverrideConfig) {
@@ -467,9 +467,7 @@ public class PinotUtils {
                 tierConfig.getString(PINOT_TIER_SEGMENT_SELECTOR_TYPE),
                 getOptionalString(tierConfig, PINOT_TIER_SEGMENT_AGE, null),
                 tierConfig.getString(PINOT_TIER_STORAGE_TYPE),
-                getOptionalString(tierConfig, PINOT_TIER_SERVER_TAG, null),
-                null,
-                null));
+                getOptionalString(tierConfig, PINOT_TIER_SERVER_TAG, null)));
       }
     }
     return tableTierConfigs;
