119b67cabd657d432e11ba294ca4e2da1389bd2d
Ronak <ronak@traceable.ai>
2023-06-28 22:36:27 +0530

    fixed spotless apply

--- view-creator-framework/src/main/java/org/hypertrace/core/viewcreator/pinot/PinotAddSchemaCommand.java
+++ view-creator-framework/src/main/java/org/hypertrace/core/viewcreator/pinot/PinotAddSchemaCommand.java
@@ -1,9 +1,7 @@
 package org.hypertrace.core.viewcreator.pinot;
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Collections;
 import java.util.List;
 import javax.annotation.Nullable;
 import org.apache.commons.lang3.StringUtils;
@@ -24,40 +22,69 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
-
 @CommandLine.Command(name = "AddSchema")
 public class PinotAddSchemaCommand extends AbstractBaseAdminCommand implements Command {
   private static final Logger LOGGER = LoggerFactory.getLogger(PinotAddSchemaCommand.class);
   static final String DEFAULT_CONTROLLER_PORT = "9000";
 
-  @CommandLine.Option(names = {"-controllerHost"}, required = false, description = "host name for controller.")
+  @CommandLine.Option(
+      names = {"-controllerHost"},
+      required = false,
+      description = "host name for controller.")
   private String _controllerHost;
 
-  @CommandLine.Option(names = {"-controllerPort"}, required = false, description = "port name for controller.")
+  @CommandLine.Option(
+      names = {"-controllerPort"},
+      required = false,
+      description = "port name for controller.")
   private String _controllerPort = DEFAULT_CONTROLLER_PORT;
 
-  @CommandLine.Option(names = {"-controllerProtocol"}, required = false, description = "protocol for controller.")
+  @CommandLine.Option(
+      names = {"-controllerProtocol"},
+      required = false,
+      description = "protocol for controller.")
   private String _controllerProtocol = CommonConstants.HTTP_PROTOCOL;
 
-  @CommandLine.Option(names = {"-schemaFile"}, required = true, description = "Path to schema file.")
+  @CommandLine.Option(
+      names = {"-schemaFile"},
+      required = true,
+      description = "Path to schema file.")
   private String _schemaFile = null;
 
-  @CommandLine.Option(names = {"-exec"}, required = false, description = "Execute the command.")
+  @CommandLine.Option(
+      names = {"-exec"},
+      required = false,
+      description = "Execute the command.")
   private boolean _exec;
 
-  @CommandLine.Option(names = {"-user"}, required = false, description = "Username for basic auth.")
+  @CommandLine.Option(
+      names = {"-user"},
+      required = false,
+      description = "Username for basic auth.")
   private String _user;
 
-  @CommandLine.Option(names = {"-password"}, required = false, description = "Password for basic auth.")
+  @CommandLine.Option(
+      names = {"-password"},
+      required = false,
+      description = "Password for basic auth.")
   private String _password;
 
-  @CommandLine.Option(names = {"-authToken"}, required = false, description = "Http auth token.")
+  @CommandLine.Option(
+      names = {"-authToken"},
+      required = false,
+      description = "Http auth token.")
   private String _authToken;
 
-  @CommandLine.Option(names = {"-authTokenUrl"}, required = false, description = "Http auth token url.")
+  @CommandLine.Option(
+      names = {"-authTokenUrl"},
+      required = false,
+      description = "Http auth token url.")
   private String _authTokenUrl;
 
-  @CommandLine.Option(names = {"-help", "-h", "--h", "--help"}, required = false, help = true,
+  @CommandLine.Option(
+      names = {"-help", "-h", "--h", "--help"},
+      required = false,
+      help = true,
       description = "Print this message.")
   private boolean _help = false;
 
@@ -80,16 +107,25 @@ public class PinotAddSchemaCommand extends AbstractBaseAdminCommand implements C
 
   @Override
   public String toString() {
-    String retString = ("AddSchema -controllerProtocol " + _controllerProtocol + " -controllerHost " + _controllerHost
-        + " -controllerPort " + _controllerPort + " -schemaFile " + _schemaFile + " -user " + _user + " -password "
-        + "[hidden]");
+    String retString =
+        ("AddSchema -controllerProtocol "
+            + _controllerProtocol
+            + " -controllerHost "
+            + _controllerHost
+            + " -controllerPort "
+            + _controllerPort
+            + " -schemaFile "
+            + _schemaFile
+            + " -user "
+            + _user
+            + " -password "
+            + "[hidden]");
 
     return ((_exec) ? (retString + " -exec") : retString);
   }
 
   @Override
-  public void cleanup() {
-  }
+  public void cleanup() {}
 
   public PinotAddSchemaCommand setControllerHost(String controllerHost) {
     _controllerHost = controllerHost;
@@ -129,8 +165,7 @@ public class PinotAddSchemaCommand extends AbstractBaseAdminCommand implements C
   }
 
   @Override
-  public boolean execute()
-      throws Exception {
+  public boolean execute() throws Exception {
     if (_controllerHost == null) {
       _controllerHost = NetUtils.getHostAddress();
     }
@@ -149,11 +184,14 @@ public class PinotAddSchemaCommand extends AbstractBaseAdminCommand implements C
 
     Schema schema = Schema.fromFile(schemaFile);
     try (FileUploadDownloadClient fileUploadDownloadClient = new FileUploadDownloadClient()) {
-      fileUploadDownloadClient.addSchema(FileUploadDownloadClient
-              .getUploadSchemaURI(_controllerProtocol, _controllerHost, Integer.parseInt(_controllerPort)),
-          schema.getSchemaName(), schemaFile, AuthProviderUtils
-              .toRequestHeaders(makeAuthProvider(_authProvider, _authTokenUrl, _authToken,
-              _user, _password)), List.of(new BasicNameValuePair("force", "true")));
+      fileUploadDownloadClient.addSchema(
+          FileUploadDownloadClient.getUploadSchemaURI(
+              _controllerProtocol, _controllerHost, Integer.parseInt(_controllerPort)),
+          schema.getSchemaName(),
+          schemaFile,
+          AuthProviderUtils.toRequestHeaders(
+              makeAuthProvider(_authProvider, _authTokenUrl, _authToken, _user, _password)),
+          List.of(new BasicNameValuePair("force", "true")));
     } catch (Exception e) {
       LOGGER.error("Got Exception to upload Pinot Schema: " + schema.getSchemaName(), e);
       return false;
@@ -162,8 +200,8 @@ public class PinotAddSchemaCommand extends AbstractBaseAdminCommand implements C
   }
 
   @Nullable
-  static AuthProvider makeAuthProvider(AuthProvider provider, String tokenUrl, String authToken, String user,
-      String password) {
+  static AuthProvider makeAuthProvider(
+      AuthProvider provider, String tokenUrl, String authToken, String user, String password) {
     if (provider != null) {
       return provider;
     }
