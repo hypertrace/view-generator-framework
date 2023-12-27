5ce7028551aa7ef8edaae4f32927797c0b2d4c04
kishansairam9 <kishan.adapa@traceable.ai>
2023-12-27 19:40:25 +0530

    update

--- view-generator-framework/build.gradle.kts
+++ view-generator-framework/build.gradle.kts
@@ -11,7 +11,7 @@ tasks.test {
 }
 
 dependencies {
-  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.62")
+  implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.64")
   implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-framework:0.4.3")
   implementation("org.hypertrace.core.kafkastreams.framework:avro-partitioners:0.2.13")
   implementation("org.apache.avro:avro:1.11.1")
