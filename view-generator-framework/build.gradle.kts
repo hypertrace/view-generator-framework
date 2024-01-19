4dc105a3858faa1d3aa54f57a3e558e9be8ba246
Aaron Steinfeld <aaron@traceable.ai>
2024-01-19 10:06:52 -0500

    chore: update dependencies and suppressions

--- view-generator-framework/build.gradle.kts
+++ view-generator-framework/build.gradle.kts
@@ -12,19 +12,19 @@ tasks.test {
 
 dependencies {
   implementation("org.hypertrace.core.serviceframework:platform-service-framework:0.1.64")
-  implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-framework:0.4.3")
-  implementation("org.hypertrace.core.kafkastreams.framework:avro-partitioners:0.2.13")
-  implementation("org.apache.avro:avro:1.11.1")
+  implementation("org.hypertrace.core.kafkastreams.framework:kafka-streams-framework:0.4.5")
+  implementation("org.hypertrace.core.kafkastreams.framework:avro-partitioners:0.4.5")
+  implementation("org.apache.avro:avro")
   implementation("com.typesafe:config:1.4.2")
-  implementation("com.google.guava:guava:32.0.1-jre")
+  implementation("com.google.guava:guava:32.1.2-jre")
 
   // Logging
-  implementation("org.slf4j:slf4j-api:2.0.5")
+  implementation("org.slf4j:slf4j-api:2.0.7")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
   testImplementation("org.junit-pioneer:junit-pioneer:2.0.0")
   testImplementation("org.mockito:mockito-core:5.2.0")
-  testImplementation("org.apache.kafka:kafka-streams-test-utils:7.2.1-ccs")
+  testImplementation("org.apache.kafka:kafka-streams-test-utils")
   testRuntimeOnly("org.apache.logging.log4j:log4j-slf4j2-impl:2.20.0")
 }
 
